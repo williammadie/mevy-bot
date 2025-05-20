31933458b0052d578fe3ea9d48e723b84c3a9140
williammadie <william.madie0@gmail.com>
2025-05-20 19:33:10 +0200

    fix vector store healthcheck

--- mevy_bot/routers/vector_store.py
+++ mevy_bot/routers/vector_store.py
@@ -7,7 +7,7 @@ router = APIRouter(prefix="/vector-store", tags=["Vector Store"])
 
 @router.get("/healthcheck")
 async def vector_store_healthcheck():
-    result = VectorStore.healthcheck()
+    result = await VectorStore.healthcheck()
     return {"status": "UP" if result else "DOWN"}
 
 
